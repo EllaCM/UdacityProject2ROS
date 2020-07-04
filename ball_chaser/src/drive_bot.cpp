8da254066588e39ed587a29f4487e5bb23d58ce6
EllaCM <min.cheng.ella@gmail.com>
2020-07-04 04:40:42 +0000

    still cannot move the robot

--- ball_chaser/src/drive_bot.cpp
+++ ball_chaser/src/drive_bot.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "drive_bot");
 	ros::NodeHandle n;
 	//"/cmd_vel"is the ros actuation topic that the message will be publish on
-	motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
+	motor_command_publisher = n.advertise<geometry_msgs::Twist>("/differential_drive_controller/cmd_vel",10);
 	
 	ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
 	ROS_INFO("Ready to send motor commands");
