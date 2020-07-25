0e9f44c42c9dce52c9b289488d69185fedfe1869
EllaCM <min.cheng.ella@gmail.com>
2020-07-25 15:14:15 +0000

    final

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -17,38 +17,37 @@ void drive_bot (double lin_x, double ang_z){
 }
 
 void process_image_callback(const sensor_msgs::Image img){
-  int white_pixel = 255;
   bool white_ball = false;
   int left_center_bdr = img.step/3;
   int right_center_bdr = 2*img.step/3;
+  int pos=0;
+  int row=0;
   for (int i=0.5*img.height*img.step; i<img.height*img.step; i++)
   {
-    int pos = i%img.step;
-    int row = i/img.height;
-    ROS_INFO("POS,color - %d, %d, %d", row, pos, img.data[i]);
-    if(img.data[i]==white_pixel){
-	if(pos>=0 && pos<left_center_bdr){
-      		drive_bot(0.5, 0.2);
-      		white_ball = true;
-      		ROS_INFO_STREAM("to the left");
-		break;
-    	} else if(pos>=left_center_bdr && pos<=right_center_bdr){
-      		drive_bot(0.5, 0);
-     		white_ball = true;
-      		ROS_INFO_STREAM("straight forward");
-      		break;
-    	} else if(pos>=right_center_bdr && pos<=img.step){
-      		drive_bot(0.5, -0.2);
-      		white_ball = true;
-     	 	ROS_INFO_STREAM("to the right");
-      		break;
-	}
-    } else {
-      drive_bot(0.0, 0.0);
-      white_ball = false;
+    
+    if(img.data[i]==(255, 255, 255)){
+     	 pos = i%img.step;
+    	 row = i/img.height;
+    	 white_ball = true;
+      	 ROS_INFO("POS - %d, %d", row, pos);
+      	 break;
     }
   }
-
+  
+  if(white_ball){
+  	if(pos>=0 && pos<left_center_bdr){
+      		drive_bot(0.3, 0.5);
+      		ROS_INFO_STREAM("to the left");
+	} else if(pos>=left_center_bdr && pos<=right_center_bdr){
+      		drive_bot(0.3, 0);
+      		ROS_INFO_STREAM("straight forward");
+    	} else if(pos>=right_center_bdr && pos<=img.step){
+      		drive_bot(0.3, -0.5);
+     	 	ROS_INFO_STREAM("to the right");
+      	}
+  }else {
+      drive_bot(0.0, 0.0);
+  }
 }
 
 int main(int argc, char** argv){
